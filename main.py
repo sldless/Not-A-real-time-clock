089377000ee422dab29d04194c6734f5034ecc00
null <60427892+vierofernando@users.noreply.github.com>
2021-10-17 12:42:46 +0700

    perf(main): performance improvements

--- main.py
+++ main.py
@@ -7,36 +7,20 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-def get_time():
+def get_time() -> tuple:
     time = datetime.now()
-    hour = time.hour
-    minute = time.minute
-    second = time.second
-    time_now = {"hour":hour, "minute": minute, "second": second}
-    return time_now
+    return (time.hour, time.minute, time.second)
 
 @app.route('/')
 def index():
-    hour = get_time().get('hour')
-    minute = get_time().get('minute')
-    second = get_time().get('second')
+    hour, minute, second = get_time()
+
     if (hour == 0):
         hour = 12
-    if (hour > 12):
-        hour = hour - 12
-        
-    if (minute < 10):
-        minute = 0 + minute
-    if (second  < 10):
-        minute = 0 + second
-        
-    context = {
-        'hour': hour,
-        'minute': minute,
-        'second': get_time().get('second')
-    }
-    context.update(**context)
-    return render_template('index.html', **context)
+    elif (hour > 12):
+        hour -= 12
+    
+    return render_template('index.html', hour=hour, minute=minute, second=second)
 
 @app.route('/refresh_data')
 def refresh_data():

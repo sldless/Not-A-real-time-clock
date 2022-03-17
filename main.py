fce3a5177de15391b4d197a931d1b1fe66001e01
sldless <67599596+sldless@users.noreply.github.com>
2022-03-16 22:45:32 -0400

    Removed it since you only need to install one package

--- main.py
+++ main.py
@@ -1,33 +1,9 @@
 from flask import Flask, render_template
-from datetime import datetime
-import requests
-from flask_socketio import SocketIO
-from flask.json import jsonify
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
-
-def get_time() -> tuple:
-    time = datetime.now()
-    return (time.hour, time.minute, time.second)
 
 @app.route('/')
 def index():
-    hour, minute, second = get_time()
-
-    if (hour == 0):
-        hour = 12
-    elif (hour > 12):
-        hour -= 12
-    
-    return render_template('index.html', hour=hour, minute=minute, second=second)
-
-@app.route('/refresh_data')
-def refresh_data():
-
-    data = requests.get('http://127.0.0.1:5000/')
-    return jsonify({'data': data.text})
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-    socketio.run(app)
